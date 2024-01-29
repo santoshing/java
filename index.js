ab567948d6b1927eb0a32da6d1bb563af2a729c9
santoshing <protoyshing@gmail.com>
2024-01-29 20:40:40 +0600

    java

---
+++ index.js
@@ -0,0 +1,14 @@
+function myfun() {
+
+       var box = document.getElementById('demo').style.display = 'none';;
+
+
+}
+
+
+
+function myfun2() {
+
+       var box = document.getElementById('demo').style.display = 'block';;
+
+}
